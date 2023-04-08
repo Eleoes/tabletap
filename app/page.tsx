4bd1473ad08ffab6288a74e14efd6b11004bccdd
eleoes <leo.escaroz@gmail.com>
2023-04-08 14:15:54 -0700

    wip: Reservation card

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
                 </li>
             </ul>
         </nav>
-        {/* MAIN NAVBAR */}
+        {/* RESTAURANT DETAILS PORTION */}
         <div className="h-96 overflow-hidden">
           <div className="bg-center bg-gradient-to-r from-[#0f1f47] to-[#5f6984] h-full flex justify-center items-center">
             <h1 className="text-7xl text-white capitalize text-shadow text-center">Tycoon Kitchen (San Francisco)</h1>
@@ -106,8 +106,42 @@ export default function Home() {
             </div>
             {/* REVIEWS */}
           </div>
+          {/* RESERVATION CARD PORTION */}
+          <div className="w-[27%] relative text-reg">
+            <div className="fixed w-[15%] bg-white rounded p-3 shadow">
+              <div className="text-center border-b pb-2 font-bold">
+                <h4 className="mr-7 text-lg">Make a reservation</h4>
+              </div>
+              <div className="my-3 flex flex-col">
+                <label htmlFor="">Party size</label>
+                <select name="" className="py-3 border-b font-light" id="">
+                  <option value="">1 person</option>
+                  <option value="">2 people</option>
+                </select>
+              </div>
+              <div className="flex justify-between">
+                <div className="flex flex-col w-[48%]">
+                  <label htmlFor="">Date</label>
+                  <input type="text" className="py-3 border-b font-light w-28" />
+                </div>
+                <div className="flex flex-col w-[48%]">
+                  <label htmlFor="">Time</label>
+                  <select className="py-3 border-b font-light">
+                    <option value="">7:30 AM</option>
+                    <option value="">8:30 AM</option>
+                    <option value="">9:30 AM</option>
+                  </select>
+                </div>
+              </div>
+              <div className="mt-5">
+                <button className="bg-red-600 rounded w-full px-4 text-white font-bold h-16">Find a Time</button>
+              </div>
+            </div>
+          </div>
+          {/* RESERVATION CARD PORTION */}
         </div>
-        {/* MAIN CONTENT */}
+        {/* RESTAURANT DETAILS PORTION */}
+        
       </main>
     </main>
     </>
